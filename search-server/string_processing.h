5ab80b95f18540b7c3387aaeb0e0aed320dd7dec
Ivan <98535185+Ivan332@users.noreply.github.com>
2023-03-08 01:42:23 +0300

    Update string_processing.h

--- search-server/string_processing.h
+++ search-server/string_processing.h
@@ -3,15 +3,17 @@
 #include <string>
 #include <vector>
 #include <set>
+#include <list>
 
-std::vector<std::string> SplitIntoWords(const std::string& text);
+std::vector<std::string_view> SplitIntoWords(const std::string_view& text);
 
 template <typename StringContainer>
-std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
-    std::set<std::string> non_empty_strings;
-    for (const std::string& str : strings) {
+std::set<std::string, std::less<>> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    std::set<std::string, std::less<>> non_empty_strings;
+    for (const std::string_view& str : strings) {
         if (!str.empty()) {
-            non_empty_strings.insert(str);
+            const std::string tmp{ str.begin(), str.end() };
+            non_empty_strings.insert(tmp);
         }
     }
     return non_empty_strings;
